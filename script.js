8cd352a229f53e6f3c98ecd255ae0a3959526510
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2024-03-29 16:41:53 +0100

    divek funkcioja mukodik

--- script.js
+++ script.js
@@ -1,62 +1,73 @@
 //kezdés
 
-
 const FODIVELEM = $(".foDiv");
 
 const NINPUTELEM = $(".N");
-let N=3
+let N = 3;
 
 const KEZDGOMBELEM = $(".gombKezd");
 
 KEZDGOMBELEM.on("click", function () {
-
-    let N=Nlekerese();
-    let meret=$(":root")
-    meret.css('--meret', N)
-    divOsszeallit(listaOsszeallit(N));
-
+  let N = Nlekerese();
+  let meret = $(":root");
+  meret.css("--meret", N);
+  divOsszeallit(listaOsszeallit(N));
+  /*  leirastKeszit(); */
 });
 
-function Nlekerese(){
-    return N = NINPUTELEM.val();
+function Nlekerese() {
+  return (N = NINPUTELEM.val());
 }
-
-function divOsszeallit(lista){
-    let txt = ``;
-  for (let index = 0; index < lista.length; index++) {
-    if(lista[index]==0){
-        txt += `<div id="${index}" class="lampa"></div>`;
-    }else{
-        txt += `<div id="${index}" class="lampa pink"></div>`;
+function divOsszeallit(lista) {
+  let txt = ``;
+  var rozsaszinekSzama = 0;
+  for (let index = 1; index < lista.length + 1; index++) {
+    if (lista[index] == 0) {
+      txt += `<div id="${index}" class="lampa"></div>`;
+    } else {
+      txt += `<div id="${index}" class="lampa pink"></div>`;
+      rozsaszinekSzama += 1;
     }
+    console.log(rozsaszinekSzama);
   }
-  FODIVELEM.html(txt)
+  FODIVELEM.html(txt);
+  return rozsaszinekSzama;
 }
 
-function listaOsszeallit(N){
-    let lista=[]
-    for (let index = 0; index < N*N; index++) {
-        let randomSzam=Math.floor(Math.random() * 2);
-        lista.push(randomSzam) 
-    }
-    
-    return lista
+function listaOsszeallit(N) {
+  let lista = [];
+  for (let index = 0; index < N * N; index++) {
+    let randomSzam = Math.floor(Math.random() * 2);
+    lista.push(randomSzam);
+  }
+
+  return lista;
 }
 
 //játék
 
-const LAMPAELEM=$('.lampa')
+const LAMPAELEM = $(".lampa");
 
 for (let index = 0; index < FODIVELEM.length; index++) {
-    FODIVELEM.eq(index).on("click", kattintas)
+  FODIVELEM.eq(index).on("click", kattintas);
 }
 
-function kattintas(event){
-    console.log(event.target.id)
-    $(event.target).toggleClass("pink")
-    FODIVELEM.eq($(event.target-1)).toggleClass("pink")
-    FODIVELEM.eq($(event.target)+1).toggleClass("pink")
-    FODIVELEM.eq($(event.target)-N).toggleClass("pink")
-    FODIVELEM.eq($(event.target)+N).toggleClass("pink")
-    
+function kattintas(event) {
+  $(event.target).toggleClass("pink");
+  /* console.log(event.target.id); */
+  const elotte1 = document.getElementById(event.target.id - 1);
+  const utana1 = document.getElementById(event.target.id - -1);
+  const elotte2 = document.getElementById(event.target.id - N);
+  const utana2 = document.getElementById(event.target.id - -N);
+  $(elotte1).toggleClass("pink");
+  $(utana1).toggleClass("pink");
+  $(elotte2).toggleClass("pink");
+  $(utana2).toggleClass("pink");
+  for (let index = 0; index < N * N.length; index++) {
+    if (LAMPAELEM.eq(index).className = "pink") {
+      rozsaszinekSzama += 1;
+    }
+    console.log(rozsaszinekSzama);
+    return rozsaszinekSzama;
+  }
 }
